9463cc9ddcc5b2780df204a5a891fdc9882fce08
Sarah Hassan <sarah.hassan@target.com>
2022-12-30 08:05:54 -0600

    removed hover  underline from my name

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
+        <h1 className={styles.maintitle}>
           Welcome to <a>Sarah.Hassan Portfolio</a>
         </h1>
 
